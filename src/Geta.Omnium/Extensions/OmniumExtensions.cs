eb3f70f5d406ce700b4bffe5ede82c5b34e66696
Marcus Vretling Pistelli <marcus@geta.no>
2021-03-19 10:21:39 +0100

    Removing demo folder.

---
+++ src/Geta.Omnium/Extensions/OmniumExtensions.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections.Generic;
+using EPiServer.Commerce.Storage;
+using Omnium.Public.Models;
+
+namespace Geta.Omnium.Extensions
+{
+    public static class OmniumExtensions
+    {
+        public static List<OmniumPropertyItem> ToPropertyList(
+            this IExtendedProperties extendedProperties)
+        {
+            var omniumPropertyItemList = new List<OmniumPropertyItem>();
+            foreach (var key in extendedProperties.Properties.Keys)
+            {
+                var str1 = key.ToString();
+                var str2 = extendedProperties.Properties[key]?.ToString();
+                var omniumPropertyItem = new OmniumPropertyItem()
+                {
+                    Key = str1,
+                    Value = str2
+                };
+                omniumPropertyItemList.Add(omniumPropertyItem);
+            }
+            return omniumPropertyItemList;
+        }
+    }
+}
